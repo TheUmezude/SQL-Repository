8082574e8338852b3a7010faa6a6e7fc1d6ac130
Nkem Umezude <46586840+TheUmezude@users.noreply.github.com>
2021-01-02 17:54:16 -0500

    Update Nkem_SQL_Repository.sql

--- Nkem_SQL_Repository.sql
+++ Nkem_SQL_Repository.sql
@@ -81,6 +81,7 @@ It is generally good practice to capitalize the keywords used in an SQL query, a
  SELECT (film_name) 
  FROM film
  WHERE film_name = 'Desperado'; -- This returns all the rows of 'film_name' column where the value of 'film_name' is 'Desperado'. Case sensitivity applies.
+ -- Strings should always have single quotes.
  /*
  Comparison operators:
  < less than
@@ -101,6 +102,75 @@ It is generally good practice to capitalize the keywords used in an SQL query, a
  SELECT * -- For some reason, when 'SELECT' is the only keyword, postgreSQL complains if the asterisk is placed in parenthesis. 
  FROM film
  WHERE rental_rate > 1 AND release_date = 2006; -- This returns all the rows of the 'film' table where the value of 'rental-rate' is greater than 1 and the 'release_date' is 2006.
+ ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+ 
+  -- The 'ORDER BY' keyword is used to sort the organization of rows based on a column value (ascending or descending order).
+ SELECT (column1, column2) 
+ FROM table_1
+ ORDER BY column_1 ASC/DESC;
+ 
+ -- Example 1:
+ SELECT address, city_id FROM address
+ ORDER BY city_id DESC; -- This returns the columns 'address' and 'city_id' from the table 'address'. 
+ -- However, their order would be according to the descending order of the integer values in the 'city_id' column.
+ -- If the 'ORDER BY' keyword is left blank, the items are automatically sorted in ascending order by default.
+ -- NOTE that you can order by more than one colum contents (Precedence flows from left to right) -- You can also order by strings, floats* and integer data contents.
+ 
+ -- Example 2:
+ SELECT store_id, first_name, last_name FROM customer
+ ORDER BY store_id DESC, first_name ASC; -- This returns the columns 'store_id', 'first_name' and 'last_name' from the table 'customer'.
+ -- The columns are returned sorted in descending order for 'store_id' and ascending order for 'first_name' -- Precedence flows from left to right.
+ --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+ 
+ -- The 'LIMIT' keyword allows for the number of rows returned to be limited -- such that the entire rows in the database associated with a particular query aren't returned.
+ -- 'LIMIT' is usually combined with the 'ORDER' statement and usually is the last command to be executed.
+ 
+ -- Example 1:
+ SELECT * FROM payment
+ ORDER BY payment_date ASC
+ LIMIT 5; -- This would ordinarily have returned all the columns and rows associated with the 'payment' table, but with the 'LIMIT' keyword at the end, it returns just the top 5 row entries.
+ 
+ -- Example 2:
+ SELECT * FROM payment
+ WHERE amount > 2.99
+ ORDER BY payment_date ASC
+ LIMIT 10; -- Returning the top 10 rows for all columns in the table 'payment' where the 'amount' column is greater than 2.99.
+
+-- Example 3:
+ SELECT * FROM payment
+ LIMIT 1; -- An interesting way to get the general layout of a particular table, without printing out the entire database. Here, just the first row is returned.
+ -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+ 
+ -- The 'BETWEEN' keyword is used for getting results within a specified range. Can be used in place of combined comparison and logical operators. For instance:
+ -- value >= low AND value <= high -- can be rewritten as -- value BETWEEN low AND high
+ -- Certain instances may also require that you use a 'NOT BETWEEN' keyword for getting entries that don't exist between a specified range.
+ -- The 'BETWEEN' keyword may also be used for dates, as long as the dates are formatted in the ISO 8601 format: 'YYYY-MM-DD'
+ 
+ -- Example 1:
+ SELECT * FROM payment
+ WHERE amount BETWEEN 4.99 AND 9.99
+ LIMIT 10; -- To return the first 10 rows from all the columns in the 'payment' table, where the amount exists between 4.99 and 9.99 i.e amount >= 4.99 AND <= 9.99
+ 
+ -- Example 2:
+ SELECT COUNT(*) FROM payment
+ WHERE payment_date BETWEEN '2007-02-01' AND '2007-02-15'; -- Returns the total number of entries in the 'payment' table where the date exist between the specified dates.
+ -- NOTE that unfortunately, the date system on SQL only goes up to the midnight time-stamp, so if time is above 00:00, it won't register, so always pay extra attention.
+ -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+ 
+ -- The 'IN' operator may be used to create a condition that checks to see if a value is included in a list of multiple entries.
+ 
+ -- Example 1:
+ SELECT * FROM customer
+ WHERE first_name IN ('Jared', 'Linda', 'Susan', 'Maria'); -- This returns all entries (rows & columns) where the 'first_name' column has entries similar to the initialized strings.
+ -- NOTE that parenthesis must always be used. Also, strings must be inside the single quotes while numeric data types should be without single quotes.
+ 
+ -- Example 2:
+ SELECT * FROM customer
+ WHERE first_name NOT IN ('Jared', 'Linda', 'Susan', 'Maria') -- This returns all entries (rows & columns) where the 'first_name' column has NO entries similar to the initialized strings.
  
  
  
